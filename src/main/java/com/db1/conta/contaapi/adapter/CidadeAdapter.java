3d06028099c53d7f8688021725beb3f08eaa03fc
Your Name <you@example.com>
2019-08-06 14:35:01 -0300

    Criado cidade service e seus métodos

--- src/main/java/com/db1/conta/contaapi/adapter/CidadeAdapter.java
+++ src/main/java/com/db1/conta/contaapi/adapter/CidadeAdapter.java
@@ -1,5 +1,8 @@
 package com.db1.conta.contaapi.adapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.db1.conta.contaapi.domain.dto.CidadeResponseDTO;
 import com.db1.conta.contaapi.domain.entity.Cidade;
 
@@ -12,5 +15,12 @@ public class CidadeAdapter {
 		responseDTO.setEstado(cidade.getEstado());
 		return responseDTO;
 	}
-
+	
+	public static List<CidadeResponseDTO> entityToResponse(List<Cidade> cidades) {
+		List<CidadeResponseDTO> cidadesResponse = new ArrayList<CidadeResponseDTO>();
+		for (Cidade cidade : cidades) {
+			cidadesResponse.add(entityToResponse(cidade));
+		}
+		return cidadesResponse;
+	}
 }
--- src/main/java/com/db1/conta/contaapi/service/CidadeService.java
+++ src/main/java/com/db1/conta/contaapi/service/CidadeService.java
@@ -1,5 +1,6 @@
 package com.db1.conta.contaapi.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,5 +34,47 @@ public class CidadeService {
 		}		
 		throw new RuntimeException("Cidade de ID" + cidadeId + " não encontrada");
 	}
-
+	
+	public List<CidadeResponseDTO> findAll() {
+		List<Cidade> cidades = cidadeRepository.findAll();
+		List<CidadeResponseDTO> response = CidadeAdapter.entityToResponse(cidades);
+		return response;
+	}
+	
+	public CidadeResponseDTO findByName(String nome) {
+		Optional<Cidade> opCidade = cidadeRepository.findByNomeOptional(nome);
+		if (opCidade.isPresent()) {
+			Cidade cidade = opCidade.get();
+			CidadeResponseDTO cidadeResponse = CidadeAdapter.entityToResponse(cidade);
+			return cidadeResponse;
+		}
+		throw new RuntimeException("Cidade de nome " + nome + " não  encontrada"); 	
+	}
+	
+	public CidadeResponseDTO findByNameJava8(String nome) {				
+		return cidadeRepository
+				.findByNomeOptional(nome)
+				.map(CidadeAdapter::entityToResponse)
+				.orElseThrow(() -> 
+					new RuntimeException("Cidade de nome " + nome + " não  encontrada"));
+	}
+	
+	public void delete(Long cidadeId) {
+		Optional<Cidade> opCidade = cidadeRepository.findById(cidadeId);
+		if (opCidade.isPresent()) {
+			Cidade cidade = opCidade.get();
+			cidadeRepository.delete(cidade);
+		} else {
+			throw new RuntimeException("Cidade de ID " + cidadeId + " não encontrada");
+		}
+	}
+	
+	public CidadeResponseDTO findById(Long cidadeId) {
+		Optional<Cidade> opCidade = cidadeRepository.findById(cidadeId);
+		if (opCidade.isPresent()) {
+			Cidade cidade = opCidade.get();
+			return CidadeAdapter.entityToResponse(cidade);
+		}
+		throw new RuntimeException("Cidade de ID " + cidadeId + " não encontrada");
+	}
 }
